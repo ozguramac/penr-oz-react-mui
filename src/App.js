8d6963a36526a5cc3fdb2f22db1387c5fe6cc111
Özgür V Amaç <ozgur.v.amac@derinworksllc.com>
2024-02-15 16:41:31 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import MaterialUIButton from './components/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Material-UI Example</h1>
+        <MaterialUIButton />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+// src/components/Button.js
+import React from 'react';
+import Button from '@mui/material/Button';
+
+function MaterialUIButton() {
+  return (
+    <Button variant="contained" color="primary">
+      Hello Material UI
+    </Button>
+  );
+}
+
+export default MaterialUIButton;
